036dda0bfefb0939455f2920c704e6d170de35cb
Wenfei Zhou <wzhou2046@gmail.com>
2020-07-06 17:33:32 -0400

    type: component body: added new components content and title

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 
+import { Content, Title } from './components'
 import { unregister } from './core'
 import { GlobalStyles, theme } from './styles'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <div>Hello world</div>
+    <Content data-cy="content">
+      <Title data-cy="title">Sudoku</Title>
+    </Content>
   </ThemeProvider>,
   document.getElementById('root')
 )
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,33 +1,34 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 export default createGlobalStyle`
+${({ theme }) => css`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }
 
-    *,
-    *::before,
-    *::after {
-        margin: 0;
-        padding: 0;
-        box-sizing: inherit;
-    }
+  html {
+    height: 100%;
 
-    html {
+    body {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      margin: 0;
+
+      #root {
+        background: ${theme.colors.background};
+        color: ${theme.colors.black};
+        display: flex;
+        font-family: sans-serif;
         height: 100%;
-
-        body {
-            display: flex;
-            flex-direction: column; 
-            height: 100%;
-            margin: 0;
-
-            #root {
-                background: radial-gradient(#282c34cc, #282c34);
-                color: #282c34;
-                display: flex;
-                font-family: sans-serif;
-                height: 100%;
-                justify-content: center;
-                padding: 15px;
-            }
-        }
+        justify-content: center;
+        padding: 15px;
+      }
     }
+  }
+`}
 `
